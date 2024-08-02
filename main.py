3c9827266389e0e00ee72288a3d904354075d5ad
Jon Gorostiola <jon.gorostiola@ge.com>
2024-08-02 10:30:48 +0200

    Main code added

---
+++ main.py
@@ -0,0 +1,29 @@
+from typing import List
+from check import check_solution
+
+###################### START THE CODE HERE ######################
+
+
+class Solution:
+
+    def countSeniors(self, input: List[str]) -> int:
+
+        return 0
+    
+    
+########################## TEST CASES ###########################
+
+test_cases = [
+    {
+        "input": 0,
+        "output": 0
+    },
+    {
+        "input": 1,
+        "output": 1
+    }
+]
+
+####################### END THE CODE HERE #######################
+
+check_solution(Solution(), test_cases)
